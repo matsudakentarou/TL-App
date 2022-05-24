0f15a613660cad74b98b299e90576300c0b22e46
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-05-24 23:10:35 +0900

    sidebar disvisible

--- TL_app/models.py
+++ TL_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from pytz import timezone
 
 class TLE(models.Model):
     title = models.CharField('タイトル', max_length=255)
-    
+    importance = models.IntegerField(default=1)
+    updated_at = models.DateTimeField('更新日時', auto_now=True)
 
     def __str__(self):
         return self.title
