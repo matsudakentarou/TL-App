b599c6e23801f915516eb5f401afbdebab5f33b6
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-05-26 15:17:37 +0900

    add end_at

--- TL_app/models.py
+++ TL_app/models.py
@@ -4,7 +4,8 @@ from pytz import timezone
 class TLE(models.Model):
     title = models.CharField('タイトル', max_length=255)
     importance = models.IntegerField(default=1)
-    updated_at = models.DateTimeField('更新日時', auto_now=True)
+    start_at = models.DateTimeField('開始日時', auto_now=True)
+    end_at = models.DateTimeField('終了日時', auto_now=True)
 
     def __str__(self):
         return self.title
