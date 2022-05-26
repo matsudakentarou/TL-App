deb3492b608dc68cc4b53d0d1ba3afb53c2d979d
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-05-26 16:42:41 +0900

    TLE datetimefield

--- TL_app/models.py
+++ TL_app/models.py
@@ -1,11 +1,23 @@
 from django.db import models
-from pytz import timezone
+from django.utils import timezone
 
 class TLE(models.Model):
-    title = models.CharField('タイトル', max_length=255)
+    title = models.CharField(max_length=255)
     importance = models.IntegerField(default=1)
-    start_at = models.DateTimeField('開始日時', auto_now=True)
-    end_at = models.DateTimeField('終了日時', auto_now=True)
+    start_at = models.DateTimeField(
+        verbose_name='',
+        editable = True,
+        blank=True,
+        null=True,
+        default = timezone.now
+    )
+    end_at = models.DateTimeField(
+        verbose_name='',
+        editable = True,
+        blank=True,
+        null=True,
+        default = timezone.now
+        )
 
     def __str__(self):
         return self.title
