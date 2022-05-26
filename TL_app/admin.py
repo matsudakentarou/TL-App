872c7fb6f2d5c63143ae3111fc1be3987e7c054d
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-05-26 17:58:20 +0900

    add TL-model

--- TL_app/admin.py
+++ TL_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import TLE
+from .models import (TLE, TL)
 
 admin.site.register(TLE)
+admin.site.register(TL)
--- TL_app/models.py
+++ TL_app/models.py
@@ -1,6 +1,26 @@
+from turtle import title
 from django.db import models
 from django.utils import timezone
 
+class TL(models.Model):
+    title = models.CharField(max_length=255)
+    english = models.CharField(max_length=255)
+    date = models.DateTimeField(
+        verbose_name='',
+        editable = True,
+        blank=True,
+        null=True,
+        default = timezone.now
+    )
+    abstract = models.TextField(default ='')
+    #img
+    body = models.TextField(default ='')
+    #inline = models.ManyToManyField
+    #outline = models.ManyToManyField
+
+
+    def __str__(self):
+        return self.title
 class TLE(models.Model):
     title = models.CharField(max_length=255)
     importance = models.IntegerField(default=1)
