6eb41453847538d8ba79bb1ddc551abc57ff49de
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-05-30 22:11:19 +0900

    get english from model

--- TL_app/views.py
+++ TL_app/views.py
@@ -10,14 +10,14 @@ class IndexView(View):
     def get(self, request, *args, **kwargs):
         parent_id = 1
         tl_title = TL.objects.filter(id=parent_id)
-        tl_english= TL.objects.filter(id=parent_id)#Englishってどうやって取り出したらいいんだっけ
+        tl_data= TL.objects.filter(id=parent_id)#Englishってどうやって取り出したらいいんだっけ
         tle_data = TLE.objects.filter(parent=parent_id)
         latest = TLE.objects.filter(parent=parent_id).aggregate(Max('end_at'))
         oldest= TLE.objects.filter(parent=parent_id).aggregate(Min('start_at'))
         return render(request, 'app/index.html', {
             'tle_data': tle_data,
             'tl_title': tl_title,
-            'tl_english': tl_english,
+            'tl_data': tl_data,
             'latest': latest['end_at__max'].year,
             'oldest': oldest['start_at__min'].year,
         })
