3918a8f248216fdd49c3531f2381aa6b88bc10b6
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-05-31 22:07:29 +0900

    year loop

--- TL_app/views.py
+++ TL_app/views.py
@@ -6,20 +6,28 @@ from django.http import JsonResponse
 from django.db.models import Max, Min
 
 
+
 class IndexView(View):
+
     def get(self, request, *args, **kwargs):
+        
         parent_id = 1
         tl_title = TL.objects.filter(id=parent_id)
         tl_data= TL.objects.filter(id=parent_id)#Englishってどうやって取り出したらいいんだっけ
         tle_data = TLE.objects.filter(parent=parent_id)
         latest = TLE.objects.filter(parent=parent_id).aggregate(Max('end_at'))
         oldest= TLE.objects.filter(parent=parent_id).aggregate(Min('start_at'))
+        for_range = [i for i in range(1800,2022)]
+        context = {
+            'for_range': for_range,
+        }
         return render(request, 'app/index.html', {
             'tle_data': tle_data,
             'tl_title': tl_title,
             'tl_data': tl_data,
             'latest': latest['end_at__max'].year,
             'oldest': oldest['start_at__min'].year,
+            'for_range': for_range,
         })
 
 
