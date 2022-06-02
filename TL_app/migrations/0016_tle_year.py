925d9b45c078f3f7426f153b625651da021c53f5
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-06-02 18:02:55 +0900

    commit

--- TL_app/migrations/0016_tle_year.py
+++ TL_app/migrations/0016_tle_year.py
@@ -15,11 +15,9 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='tle',
             name='year',
-<<<<<<< HEAD
+            
             field=models.ForeignKey(blank=True, default=1, on_delete=django.db.models.deletion.CASCADE, to='TL_app.year'),
             preserve_default=False,
-=======
-            field=models.ForeignKey(default=1800, on_delete=django.db.models.deletion.DO_NOTHING, to='TL_app.tl_year'),
->>>>>>> dd8416c3aebdffd5ab6cf222807d61fbb924d9ac
+
         ),
     ]
