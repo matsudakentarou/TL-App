329df62c3ebe28b54f8c985daefed6ff14d0a632
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-06-05 22:38:25 +0900

    list change

--- TL_app/views.py
+++ TL_app/views.py
@@ -9,19 +9,14 @@ class IndexView(View):
     def get(self, request, *args, **kwargs):
         parent_id = 1
         tl_data= TL.objects.filter(id=parent_id)
-        year = Year.objects.filter(TL=parent_id)
         tle_data = TLE.objects.order_by('start_at').filter(parent=parent_id)
         latest = TLE.objects.filter(parent=parent_id).aggregate(Max('end_at'))
         oldest= TLE.objects.filter(parent=parent_id).aggregate(Min('start_at'))
         for_range = [i for i in range(1800,2022)]
-        context = {
-            'for_range': for_range,
-        }
 
         return render(request, 'app/index.html', {
             'tle_data': tle_data,
             'tl_data': tl_data,
-            'year': year,
             'latest': latest['end_at__max'].year,
             'oldest': oldest['start_at__min'].year,
             'for_range': for_range,
