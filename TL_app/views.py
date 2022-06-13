59bf87f09c1528d69145be005fbd2692209dd88c
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-06-13 20:43:56 +0900

    create TLE

--- TL_app/views.py
+++ TL_app/views.py
@@ -1,27 +1,24 @@
 from multiprocessing import parent_process
+from django.urls import reverse_lazy
 from django.views.generic import View
 from .models import (TLE, TL)
 from django.http import JsonResponse
 from django.db.models import Max, Min
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, resolve_url
 import json
 from django.forms import model_to_dict
 from .forms import TleForm
+from django.views.generic import CreateView
 
-class IndexView(View):
+
+class IndexView(CreateView):
+
     def get(self, request, *args, **kwargs):
-        parent_id = 3
+        parent_id = 1
         form = TleForm()
         tl_data= TL.objects.filter(id=parent_id)
         tle_data = TLE.objects.order_by('start_at').filter(parent=parent_id)
         for_range = [i for i in range(1800,2022)]
-
-        if request.headers.get("Content-Type") == "application/json":
-            # すべてのtleを辞書型で受け取る
-            tles = TLE.objects.values()
-            tles_list = list(tles)
-            # json形式でレスポンスを返す
-            return JsonResponse(tles_list, safe=False, status=200)
 
         return render(request, 'app/index.html', {
             'tle_data': tle_data,
@@ -29,3 +26,10 @@ class IndexView(View):
             'for_range': for_range,
             'form':form,
         })
+
+    def get_success_url(self):
+        model = TLE
+        form_class = TleForm
+        success_url = reverse_lazy('index')
+        return resolve_url('index')
+
