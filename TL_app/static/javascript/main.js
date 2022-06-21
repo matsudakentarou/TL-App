5d07ee06bc4f68bf26e80eff20373cdb57a5e8b5
matsudakentarou <84513285+matsudakentarou@users.noreply.github.com>
2022-06-21 19:19:22 +0900

    javascript shadow

--- TL_app/static/javascript/main.js
+++ TL_app/static/javascript/main.js
@@ -2,18 +2,21 @@ const sidebar = document.getElementById("sidebar");
 
 function TLClick(){
 	document.getElementById("sidebar").style.display="block";
+	document.getElementById("shadow").style.display="block";
 	const tab1 = document.getElementById( "tab1" ) ;
 	document.getElementById("menu-btn-check").checked = false;
 	tab1.checked = true ;
 }
 function TLEClick(){
 	document.getElementById("sidebar").style.display="block";
+	document.getElementById("shadow").style.display="block";
 	const tab2 = document.getElementById( "tab2" ) ;
 	document.getElementById("menu-btn-check").checked = false;
 	tab2.checked = true ;
 }
 function addTLEClick(){
 	document.getElementById("sidebar").style.display="block";
+	document.getElementById("shadow").style.display="block";
 	const tab3 = document.getElementById( "tab3" ) ;
 	document.getElementById("menu-btn-check").checked = false;
 	tab3.checked = true ;
@@ -21,5 +24,6 @@ function addTLEClick(){
 
 function sidebarhide(){
 	document.getElementById("sidebar").style.display="none";
+	document.getElementById("shadow").style.display="none";
 }
 
